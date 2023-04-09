4ccc6b7f82b7965fc68c183823236f9d6acbfa99
PikaMaria27 <93082923+PikaMaria27@users.noreply.github.com>
2023-04-09 18:02:33 +0300

    Add files via upload

---
+++ pages/indexProj.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Welcome to MaMa!</title>
+	<link rel = "stylesheet" href = "main_style.css" />
+	<link rel = "stylesheet" href = "index_style.css" />
+	<style>
+	* { font-family: cambria; font-size: 17px;}
+	label { display: block; }
+	</style>
+</head>
+<body>
+	<ul class="titles">
+        <li><a href="../indexProj.php" id="logo"><img src="newLogo.jpg" alt="logo icon"> </a></li>
+        <li id="buttonLog"> <a href="Login.html"> Sign Out </a> </li> 
+    </ul>
+
+	<article class="presentPage">
+		<div>
+			<img src="newLogo.jpg" alt="MaMa! Logo" width=450px, height=auto>
+		</div>
+		<div>
+			<header class="presentationTitle">
+				MaMa! - Manners Matter
+			</header>
+			<p class="presentationBody">
+				Welcome to MaMa - Manners Matter! Our site is dedicated to teaching you 
+				the essential manners and etiquette for virtual contexts such as online 
+				meetings and social media interactions. With our expert guidance, you'll 
+				learn how to use emojis, communicate effectively, and make a great impression 
+				in any virtual setting. Join us on the journey to becoming a polite and 
+				well-mannered virtual citizen today!
+			</p>
+		</div>
+	</article>
+
+	<article class="categs">
+		<header>
+			<form method="POST" action="scoreboardProj.php">
+    			<input class="goToPage" type="submit" value="My Account"/>
+  			</form>
+		</header>
+		<p>
+			This is where you can see information about your account.
+		</p>
+	</article>
+
+	<article class="categs">
+		<header>
+			<form method="POST" action="lessonsProj.php">
+    			<input class="goToPage" type="submit" value="Go To Lessons"/>
+  			</form>
+		</header>
+		<p>
+			This is where you can start learning.
+		</p>
+	</article>
+
+	<article class="categs">
+		<header>
+			<form method="POST" action="scoreboardProj.php">
+    			<input class="goToPage" type="submit" value="Suggestions For You"/>
+  			</form>
+		</header>
+		<p>
+			This is where you can get information about manners based on your needs.
+		</p>
+	</article>
+
+	<article class="categs">
+		<header>
+			<form method="POST" action="scoreboardProj.php">
+    			<input class="goToPage" type="submit" value="Scoreboard"/>
+  			</form>
+		</header>
+		<p>
+			This is where you can see the leaderboard.
+		</p>
+	</article>
+</body>
+</html>
