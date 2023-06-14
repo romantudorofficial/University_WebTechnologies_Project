ff2db750697ab117e54b5e42d8718567d72681b4
PikaMaria27 <mariagheorghiu27@ymail.com>
2023-06-14 17:21:54 +0300

    Made the existing pages responsive

--- pages/admin/delete_mvc/delete_view.php
+++ pages/admin/delete_mvc/delete_view.php
@@ -2,7 +2,7 @@
 <php>
 
     <head>
-        <meta charset="UTF-8" />
+        <meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1.0"/>
         <title> Emoji Tutorial </title>
         
         <link rel="stylesheet" href="../../styles/main_style.css" />
